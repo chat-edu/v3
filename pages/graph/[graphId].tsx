54b136c0d6856a0a3b87c1c69c69379d9c444db2
json <jason@hedmans.org>
2024-04-29 04:26:10 -0500

    update favicon

--- pages/graph/[graphId].tsx
+++ pages/graph/[graphId].tsx
@@ -15,7 +15,7 @@ const GraphPage = ({ graphId } : {graphId: string}) => {
                 <meta name="description" content="Generative learning"/>
                 <meta name="viewport"
                       content="width=device-width, height=device-height,  initial-scale=1.0, user-scalable=no, user-scalable=0;"/>
-                <link rel="icon" href="/logo.ico"/>
+                <link rel="icon" href="/favicon.ico"/>
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png"/>
                 <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon-192x192.png"/>
             </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const HomePage: NextPage = () => {
                 <meta name="description" content="A second brain for stucents"/>
                 <meta name="viewport"
                       content="width=device-width, height=device-height,  initial-scale=1.0, user-scalable=no, user-scalable=0;"/>
-                <link rel="icon" href="/logo.ico"/>
+                <link rel="icon" href="/favicon.ico"/>
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png"/>
                 <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon-192x192.png"/>
             </Head>
